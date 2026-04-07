182c7036dbf05fbbb1fbf3a520acd204d6b10eb7
leopold <leopold.schreiber@elbflorace.de>
2026-04-07 18:05:51 +0200

    added Core.hpp with platform specific assertions

---
+++ engine/src/core/Core.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <signal.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#define METEOR_ENABLE_ASSERTS
+
+#ifdef __linux__
+    #define METEOR_PLATFORM_LINUX
+#elifdef _WIN32
+    #define METEOR_PLATFORM_WINDOWS
+#endif
+
+#ifdef METEOR_ENABLE_ASSERTS
+    #ifdef METEOR_PLATFORM_WINDOWS
+        #define METEOR_ASSERT(x, ...) {if(!x) METEOR_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak();}
+        #define METEOR_CORE_ASSERT(x, ...) {if(!x) METEOR_CORE_ERROR("Assertion failed: {}", __VA_ARGS__); __debugbreak();}
+    #elifdef METEOR_PLATFORM_LINUX
+        #define METEOR_ASSERT(x, ...) {if(!x) METEOR_ERROR("Assertion failed: {}", __VA_ARGS__); assert(false);}
+        #define METEOR_CORE_ASSERT(x, ...) {if(!x) METEOR_CORE_ERROR("Assertion failed: {}", __VA_ARGS__); assert(false);}
+    #endif
+#else
+    #define METEOR_ASSERT(x, ...)
+    #define METEOR_CORE_ASSERT(x, ...)
+#endif
